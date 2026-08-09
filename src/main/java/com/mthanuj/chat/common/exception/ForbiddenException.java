d9350e0c545b5ffea9d59bf8d0e7880d5c2cb822
Thanuj Mullaguri <thanuj_mullaguri@apple.com>
2026-08-09 15:21:16 +0530

    feat(common): add API response and global exception handling

---
+++ src/main/java/com/mthanuj/chat/common/exception/ForbiddenException.java
@@ -0,0 +1,9 @@
+package com.mthanuj.chat.common.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/mthanuj/chat/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.mthanuj.chat.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
