2f97ebf71287abf4a2a39b6e67cc62586a6c23fd
Thanuj Mullaguri <thanuj_mullaguri@apple.com>
2026-08-09 07:59:58 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/mthanuj/chat/ChatApplicationBackend.java
@@ -0,0 +1,13 @@
+package com.mthanuj.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationBackend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplicationBackend.class, args);
+    }
+
+}
